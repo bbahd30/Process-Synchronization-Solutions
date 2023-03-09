4758c26ed9ea462ae5b6b8abd30c37161087fe3e
Prerna <101263466+prernaa23@users.noreply.github.com>
2023-03-10 00:50:24 +0530

    Update starveFreeBridgeSoln.cpp

--- BridgeCrossingProblem/starveFreeBridgeSoln.cpp
+++ BridgeCrossingProblem/starveFreeBridgeSoln.cpp
@@ -61,7 +61,7 @@ void *rightCar(void *arg)
             pthread_mutex_lock(&mutex);
             carsOnBridge--;
             leavingBridge(carId, 1);
-            // after crossing the bridge need to take care if the last one on bridge, then have to check check both the sides
+            // after crossing the bridge need to take care if the last one on bridge, then have to check both the sides
             // to prevent starvation, should look the number of cars on the other side waiting, if no one of left, then right-moving cars move
             if (carsOnBridge == 0 && carsToCross[0] > 0)
                 currentDirection = -1;
