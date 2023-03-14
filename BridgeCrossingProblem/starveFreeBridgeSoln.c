8fd0587caea25bc87df53e601a72cd18e9c2663d
Prerna <prerna181401@gamil.com>
2023-03-14 19:50:20 +0530

    include semaphore file

--- BridgeCrossingProblem/starveFreeBridgeSoln.c
+++ BridgeCrossingProblem/starveFreeBridgeSoln.c
@@ -3,39 +3,9 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <stdatomic.h>
+#include "../Semaphore.h"
 
-struct Semaphore
-{
-    volatile atomic_int value;
-    volatile atomic_flag mutex;
-
-} emutex,mutex;
-
-void sem_wait(struct Semaphore *s)
-{
-    while (atomic_flag_test_and_set(&s->mutex))
-        ;
-    while (atomic_load(&s->value) <= 0)
-        ;
-    atomic_fetch_sub(&s->value, 1);
-    atomic_flag_clear(&s->mutex);
-}
-
-void sem_init(struct Semaphore *s, int value)
-{
-    atomic_init(&s->value, value);
-}
-
-void sem_post(struct Semaphore *s)
-{
-    atomic_fetch_add(&s->value, 1);
-}
-
-void sem_destroy(struct Semaphore *s){
-    while(!(s->value))sem_post(s);
-}
-
-
+struct Semaphore emutex, mutex;
 int numCars;
 int rightCars, leftCars;
 int carsOnBridge;
