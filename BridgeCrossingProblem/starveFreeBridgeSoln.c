a80d4bf139aebce982a82f8f2ca2fcf9f9339be1
bbahd30 <b_bonal@cs.iitr.ac.in>
2023-03-14 21:21:25 +0530

    Add description of code

--- BridgeCrossingProblem/starveFreeBridgeSoln.c
+++ BridgeCrossingProblem/starveFreeBridgeSoln.c
@@ -5,56 +5,79 @@
 #include <stdatomic.h>
 #include "../Semaphore.h"
 
+// semaphore emutex -> provides priority access to the car arrived first
+//      releases when allowed to cross bridge (the critical section)
+//      else waits by looping without releasing the semaphore emutex
+
+// semaphore mutex -> allows to update the shared variable carsOnBridge, when satisfies the conditon
+//      to prevent the race conditon among the cars on the same side allowed to cross, for updating the value of number of cars on bridge
 struct Semaphore emutex, mutex;
 int numCars;
 int rightCars, leftCars;
 int carsOnBridge;
 int currentDirection;
 
+// functions for stimulating car crossing, waiting, arriving and leaving the bridge.
 void crossingBridge(int carId, int direction)
 {
-    if(direction==1){
-        printf("Car %d (R) ---> ---> crossing.\n",carId);
+    if (direction == 1)
+    {
+        printf("Car %d (R) ---> ---> crossing.\n", carId);
     }
-    else{
-        printf("Car %d \t\t\t\t(L) <--- <--- crossing.\n",carId);
+    else
+    {
+        printf("Car %d \t\t\t\t(L) <--- <--- crossing.\n", carId);
     }
 }
 
 void waitingCar(int carId, int direction)
 {
-    if(direction==1){
-        printf("Car %d (R) ---> ---> waiting.\n",carId);
+    if (direction == 1)
+    {
+        printf("Car %d (R) ---> ---> waiting.\n", carId);
     }
-    else{
-        printf("Car %d \t\t\t\t(L) <--- <--- waiting.\n",carId);
+    else
+    {
+        printf("Car %d \t\t\t\t(L) <--- <--- waiting.\n", carId);
     }
 }
 
 void bridgeArrived(int carId, int direction)
 {
-    if(direction==1){
-        printf("Car %d (R) ---> ---> arrived.\n",carId);
+    if (direction == 1)
+    {
+        printf("Car %d (R) ---> ---> arrived.\n", carId);
     }
-    else{
-        printf("Car %d \t\t\t\t(L) <--- <--- arrived.\n",carId);
+    else
+    {
+        printf("Car %d \t\t\t\t(L) <--- <--- arrived.\n", carId);
     }
 }
 
 void leavingBridge(int carId, int direction)
 {
-    if(direction==1){
-        printf("Car %d (R) ---> ---> leaving.\n",carId);
+    if (direction == 1)
+    {
+        printf("Car %d (R) ---> ---> leaving.\n", carId);
     }
-    else{
-        printf("Car %d \t\t\t\t(L) <--- <--- leaving.\n",carId);
+    else
+    {
+        printf("Car %d \t\t\t\t(L) <--- <--- leaving.\n", carId);
     }
 }
 
 void *rightCar(void *arg)
 {
     int carId = *(int *)arg;
+    // as the right car arrives its shows its arrival
     bridgeArrived(carId, 1);
+    // after arriving tries to aquire the emutex semaphore to enter the bridge
+    // thus tracking the car arrived first by being aquired by that thread
+
+    // is aquired by the car if there are no other cars waiting for their turn to cross the bridge
+    // as if a car is not allowed to cross, it waits without releasing the mutex semaphore
+    // thus preventing the cars arriving after the waiting car
+    // hence, prevents starvation of the car on opposite side
     sem_wait(&emutex);
     while (1)
     {
@@ -67,7 +90,6 @@ void *rightCar(void *arg)
             currentDirection = 1;
             sem_post(&mutex);
             crossingBridge(carId, 1);
-            
 
             sleep(2);
 
@@ -129,10 +151,9 @@ void *leftCar(void *arg)
 int main()
 {
     printf("Enter the number of cars: ");
-    scanf("%d",&numCars);
+    scanf("%d", &numCars);
     printf("Enter the number of cars heading towards left: ");
-    scanf("%d",&leftCars);
-
+    scanf("%d", &leftCars);
 
     rightCars = numCars - leftCars;
 
