3d73b8868e68b7eff978d4b48ff807746e6dd3e4
NehaGujar1 <gujarnehap@gmail.com>
2023-03-15 10:32:15 +0530

    Use semaphores in place of mutex

--- ProducersConsumersProblem/cyclic.c
+++ ProducersConsumersProblem/cyclic.c
@@ -6,13 +6,13 @@
 #define Max 8
 #define SizeofBuffer 10
 
-struct Semaphore not_full;
+struct Semaphore mutex;
 struct Semaphore not_empty;
 int in =0;
 int out = 0;
 static int* buffer;
 
-pthread_mutex_t mutex;
+// pthread_mutex_t mutex;
 
 void Producer_Process(void *producer_no)
 {
@@ -21,12 +21,14 @@ void Producer_Process(void *producer_no)
     {
         // sem_wait(&not_full);
         object = rand(); 
-        pthread_mutex_lock(&mutex);
+        // pthread_mutex_lock(&mutex);
+        sem_wait(&mutex);
         buffer[in] = object;
         printf("Producer %d Inserted object %d at %d \n",*((int *)producer_no), buffer[in],in);
         in = in+1;
         in = in%SizeofBuffer;
-        pthread_mutex_unlock(&mutex);
+        // pthread_mutex_unlock(&mutex);
+        sem_post(&mutex);
         sem_post(&not_empty); //signals notempty      
     }
 }
@@ -36,13 +38,15 @@ void Consumer_Process(void *consumer_no)
     for(int i=0;i<Max;i++)
     {
         sem_wait(&not_empty);
-        pthread_mutex_lock(&mutex);
+        // pthread_mutex_lock(&mutex);
+        sem_wait(&mutex);
         int object = buffer[out];
         printf("Consumer %d Removed object %d at %d \n",*((int *)consumer_no), buffer[out],out);
         buffer[out] = -1;
         out = out+1;
         out = out%SizeofBuffer;
-        pthread_mutex_unlock(&mutex);
+        // pthread_mutex_unlock(&mutex);
+        sem_post(&mutex);
         //sem_post(&not_full); //signals not_full
     }
 }
@@ -51,7 +55,8 @@ int main()
 {
     pthread_t producers[8];
     pthread_t consumers[8];
-    pthread_mutex_init(&mutex,NULL);
+    // pthread_mutex_init(&mutex,NULL);
+    sem_init(&mutex,1);
     // sem_init(&not_full, 0, SizeofBuffer);
     sem_init(&not_empty, 0);
     buffer = (int*)malloc(SizeofBuffer * sizeof(int));
@@ -73,7 +78,8 @@ int main()
         pthread_join(consumers[i], NULL);
     }
 
-    pthread_mutex_destroy(&mutex);
+    // pthread_mutex_destroy(&mutex);
+    sem_destroy(&mutex);
     sem_destroy(&not_empty);
     // sem_destroy(&not_full);
 
